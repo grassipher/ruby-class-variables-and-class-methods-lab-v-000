261db6758dbf36ac953c5a81a14c977dea63c5f3
Gabriel Valle <gvalle91@gmail.com>
2018-06-16 18:07:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,13 @@
 class Song
   @@song_count = 0
-  @@song_artists = []
-  @@song_genres = []
+  @@song_artists = {}
+  @@song_genres = {}
   
-  def initialize
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(song_name, song_artist, song_genre)
+    @@song_count += 1
+    
     
   end
 end
